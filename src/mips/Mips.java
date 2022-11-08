ddc6badd6202b7ef715518e0fe22a04e34bff356
Lokirovskii <719220502@qq.com>
2022-11-08 20:53:42 +0800

    小优化

--- src/mips/Mips.java
+++ src/mips/Mips.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Mips {
     String code;
-    String op, x1, x2, x3; // 对于 .data 段的数据，这几个字段是无意义的
+    String[] args = new String[4]; // 对于 .data 段的数据，这几个字段是无意义的
 
     private static final String splitRegex = "[a-zA-Z_][a-zA-Z0-9_]*|\\$zero|\\$..|-?\\d+";
     private static final Pattern pattern = Pattern.compile(splitRegex);
@@ -14,10 +14,10 @@ public class Mips {
         this.code = code;
         Matcher matcher = pattern.matcher(code);
         while (matcher.find()) {
-            if (op == null) op = matcher.group();
-            else if (x1 == null) x1 = matcher.group();
-            else if (x2 == null) x2 = matcher.group();
-            else if (x3 == null) x3 = matcher.group();
+            if (args[0] == null) args[0] = matcher.group();
+            else if (args[1] == null) args[1] = matcher.group();
+            else if (args[2] == null) args[2] = matcher.group();
+            else if (args[3] == null) args[3] = matcher.group();
         }
     }
 }
--- src/mips/MipsOptimizer.java
+++ src/mips/MipsOptimizer.java
@@ -11,32 +11,52 @@ class MipsOptimizer {
     private static void peepHole(NodeList<Mips> mips) {
         Wrap<Boolean> inText = new Wrap<>(false);
         mips.forEachNode(p -> {
+            // 跳过 .data
             if (p.get().code.equals(".text")) {
                 inText.set(true);
                 return;
             }
             if (!inText.get()) return;
+
+            String[] args = p.get().args;
             // 优化 lw+sw 操作
             // lw+sw->lw; sw+lw->sw
             // sw $t1,a + lw $t2,a -> sw $t1,a + move $t2,$t1
             if (p.get(1) != null) {
-                if (p.get().op.equals("lw") && p.get(1).op.equals("sw") || p.get().op.equals("sw") && p.get(1).op.equals("lw")) {
-                    if (p.get().x1.equals(p.get(1).x1) && p.get().x2.equals(p.get(1).x2) && p.get().x3.equals(p.get(1).x3)) {
+                if (args[0].equals("lw") && p.get(1).args[0].equals("sw") || args[0].equals("sw") && p.get(1).args[0].equals("lw")) {
+                    if (args[1].equals(p.get(1).args[1]) && args[2].equals(p.get(1).args[2]) && args[3].equals(p.get(1).args[3])) {
                         p.delete(1);
                     }
                 }
-                if (p.get().op.equals("sw") && p.get(1).op.equals("lw")) {
-                    if (p.get().x2.equals(p.get(1).x2) && p.get().x3.equals(p.get(1).x3)) {
-                        p.set(new Mips(String.format("move %s, %s", p.get(1).x1, p.get(0).x1)), 1);
+                if (args[0].equals("sw") && p.get(1).args[0].equals("lw")) {
+                    if (args[2].equals(p.get(1).args[2]) && args[3].equals(p.get(1).args[3])) {
+                        p.set(1, new Mips(String.format("move %s, %s", p.get(1).args[1], args[1])));
                     }
                 }
             }
-            // 删除无用的 move $t, $t
-            if (p.get().op.equals("move")) {
-                if (p.get().x1.equals(p.get().x2)) {
+            // 删除无用的指令
+            // jr $ra + jr $ra
+            if (p.get(1) != null) {
+                if (p.get().code.equals("jr $ra") && p.get().code.equals(p.get(1).code)) {
+                    p.delete();
+                }
+            }
+            // move $t, $t
+            if (args[0].equals("move")) {
+                if (args[1].equals(args[2])) {
+                    p.delete();
+                }
+            }
+            // add|sub $t, $t, 0
+            if (args[0].equals("add") || args[0].equals("sub")) {
+                if (args[1].equals(args[2]) && isZero(args[3])) {
                     p.delete();
                 }
             }
         });
+    }
+
+    private static boolean isZero(String str) {
+        return str.equals("0") || str.equals("-0") || str.equals("$0") || str.equals("$zero");
     }
 }
